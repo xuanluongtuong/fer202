5ac7e0e827fa28d31fbcaf5be1c989f98bf25e07
xuanluongtuong <xuanluongtuong@gmail.com>
2024-05-08 14:00:46 +0700

    second commit

---
+++ Exercises/ES6/package10052024/1_arrow_function.js
@@ -0,0 +1,38 @@
+//phuong phap truyen thong
+function Add(a, b) {
+    return a + b;
+}
+
+//su dung arrow function
+let Add1 = (a, b) => a + b;
+
+let Add2 = (a,b) => {
+    if(a<=0 || b<= 0){
+        console.log("a, b must be greater than zero");
+    }else {
+        return a+b;
+    }
+}
+
+let showName = () => {
+    let name = "Tuong Xuan Luong";
+    console.log("Name is: "+name);
+}
+
+console.log("Add = " + Add(10, 20));
+console.log("Add = " + Add1(10, 20));
+console.log("Add = " + Add2(10, 20));
+
+showName();
+
+let getAge = age => console.log("Age is: " + age);
+
+//Arrow function within Object literal
+let person = {
+    fName : "Tho",
+    lName : "Pham Ngoc",
+    fullName : function (){console.log(`Full name is:${this.fName} ${this.lName}`)}
+};
+
+getAge(25);
+person.fullName()
---
+++ Exercises/ES6/package10052024/2_let_const.js
@@ -0,0 +1,10 @@
+function sumNumbers(n){
+    let sum= 0;
+    for (let i = 0; i <= n; i++ ){
+         sum+=i;
+    }
+    console.log(sum);
+    console.log();
+}
+
+sumNumbers();
